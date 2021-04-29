eb0e130fd9f74b5e7528a7e223cc25cc29d8a604
jambo <hisllam35@gmail.com>
2021-04-29 15:28:24 +0300

    add number handler relise

--- script.js
+++ script.js
@@ -4,19 +4,29 @@ function cryptText(string) {
     string = string.split(' ');
     let result = ''
     string.forEach(function(item) {
-        if (item.length > 2) {
-            for (let i = 0; i < letters.length; i++) {
-                if (letters[i] === item[0].toLowerCase()) {
-                    return result = result + 'код' + item + " ";
+        if (isNaN(Number(item))) {
+            if (item.length > 2) {
+                for (let i = 0; i < letters.length; i++) {
+                    if (letters[i] === item[0].toLowerCase()) {
+                        result = result + 'код' + item + " ";
+                    }
                 }
+                result = result + 'инту' + item + " ";
+            } else {
+                result = result + item + 'ик' + " ";
             }
-            return result = result + 'инту' + item + " ";
         } else {
-            return result = result + item + 'ик' + " ";
+            let number = '';
+            if (item.length > 2) {
+                number = item[0] + item + item[item.length - 1];
+                result = result + number + ' ';
+            } else {
+                number = item[1] + item[0];
+                result = result + number + ' ';
+            }
         }
-
     });
     return result;
 }
 
-console.log(cryptText('я и он теперь в интукод'))
+console.log(cryptText('я и он теперь в интукод 12'))
